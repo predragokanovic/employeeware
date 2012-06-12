420c079d9aff28cfeef487edf1c6588490a5f0ae
Rajiv Pant <rajiv@rajiv.com>
2012-06-11 22:02:29 -0400

    fixed bug in username/password reset link. minor cosmetic changes to orgchart view.

--- employeeware/urls.py
+++ employeeware/urls.py
@@ -10,15 +10,16 @@ urlpatterns = patterns('',
 	url(r'^employees/', 'orgchartapp.views.byEmployee'),	
 	url(r'^orgchart/(?P<manager_id>\d+)$', 'orgchartapp.views.byReportingRelationship'),
 
+	url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name='admin_password_reset'),
+	(r'^admin/password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
+	(r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
+	(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
+
 	# Uncomment the admin/doc line below to enable admin documentation:
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
 	# Uncomment the next line to enable the admin:
 	url(r'^admin/', include(admin.site.urls)),
 	
-	url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name='admin_password_reset'),
-	(r'^admin/password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-	(r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
-	(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
 	
 )
