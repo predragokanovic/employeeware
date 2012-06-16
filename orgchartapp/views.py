f1d391add1a248fab0f338265768cfc954f10296
Rajiv Pant <rajiv@rajiv.com>
2012-06-16 15:43:19 -0400

    simplified template management using inheritance. added edit links in employees list.

--- orgchartapp/views.py
+++ orgchartapp/views.py
@@ -2,6 +2,7 @@ from django.template import Context, loader
 from orgchartapp.models import Employee, ReportingRelationship
 from django.http import HttpResponse
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -11,6 +12,7 @@ def homepage(request):
 	})
 	return HttpResponse(t.render(c))
 
+#@login_required
 def byEmployee(request):
 	employee_list = Employee.objects.all().order_by('full_name')
 	t = loader.get_template('orgchart/employees.html')
@@ -18,7 +20,8 @@ def byEmployee(request):
 	'employee_list': employee_list,
 	})
 	return HttpResponse(t.render(c))
-		
+
+#@login_required
 def byReportingRelationship(request, manager_id):
 	reporting_relationship_list = ReportingRelationship.objects.filter(Q(supervisor_id = manager_id) | Q(employee_id = manager_id)).order_by('dotted', 'employee__full_name')
 	primary_supervisor_id = ReportingRelationship.objects.filter(Q(employee_id = manager_id))[0].supervisor_id
